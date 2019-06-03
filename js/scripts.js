8be215f65c7a9137ea6868137c0ac577d44738a3
Serhiy Savchuk and Tamara Trefilova <tamara@>
2019-06-03 10:26:46 -0700

    Added new functions multiplication, division, subtraction, addition

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
-var sayResult = function (someResult) {
- alert(someResult);
+var add = function(number1, number2){
+  return number1+number2;
 }
 
-sayResult(prompt("What is your name?")+" is "+prompt("What is your age?")+" and likes "+prompt("What is your favorite food?"));
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: "));
+var result = add(number1, number2);
+alert(result);
+
+var subtract = function(number1, number2){
+  return number1-number2;
+}
+
+var result = subtract(number1, number2);
+alert(result);
+
+var multiply = function(number1, number2){
+  return number1*number2;
+  }
+
+var result = multiply(number1, number2);
+alert(result);
+
+var divide = function(number1, number2){
+  return number1/number2;
+}
+
+var result = divide(number1,number2);
+alert(result);

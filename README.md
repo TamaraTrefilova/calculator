37b9f676a29c59a9c2874149576cb01df0f92b6c
Serhiy Savchuk and Tamara Trefilova <tamara@>
2019-06-03 10:09:15 -0700

    Add initial  index.html, scripts.js, and README.

---
+++ README.md
@@ -0,0 +1,9 @@
+## Calculator
+_by Tamara and Serhiy June 3, 2019_
+
+## Technologies
+* HTML
+* CSS
+* JavaScript
+
+Copyright Epicodus (c) 2019
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var sayResult = function (someResult) {
+ alert(someResult);
+}
+
+sayResult(prompt("What is your name?")+" is "+prompt("What is your age?")+" and likes "+prompt("What is your favorite food?"));
